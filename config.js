0f166087bfd32a048c9d41ddb05c4e2786902b11
mps233 <miaona233@gmail.com>
2023-06-26 02:38:43 +0800

    change text

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.__PRE_CONFIG__ = {
-  header: 'Server Status',
-  subHeader: 'Servers\' Probes Set up with ServerStatus',
+  header: 'Bocchi Status',
+  subHeader: '我们不断监控我们的基础设施及其相关服务',
   interval: 1.5,
-  footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
+  footer: '<p>Powered by <a href="https://bocchi.ooo">Bocchi</a></p>'
 };
